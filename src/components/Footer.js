fc3f608c017d5f8d50e0c5e3e51619dd14fed347
mehdibourahla <mehdibrhl4@gmail.com>
2021-04-07 18:41:11 +0100

    - Fixed Footer responsiveness.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='relative bg-black text-white'>
       <div className='flex flex-col items-center text-center py-16'>
         <h1 className='text-4xl font-bold'>Follow me on Social Media!</h1>
-        <div className='flex justify-center items-center h-48 space-x-32'>
+        <div className='flex justify-center items-center h-48 space-x-6 md:space-x-32'>
           <a href='https://www.youtube.com/channel/UCmj1p0fQNivPBc-eZNxmNMQ'>
             <i className='fab fa-youtube fa-3x hover:text-red'></i>
           </a>
@@ -25,7 +25,7 @@ const Footer = () => {
           className='hover:text-4xl p-4 text-3xl text-red border border-red rounded-lg'
         ></Button>
       </div>
-      <div className="absolute w-full text-center bottom-0 pb-2">
+      <div className="text-center bottom-0 pb-2">
         <p>Designed and Developed by Mehdi Bourahla.</p>
       </div>
     </div>
